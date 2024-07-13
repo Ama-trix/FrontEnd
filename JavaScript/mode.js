306fbcf71a1e734aafe4008375862ea6a2dce996
Gauri Sharma <gaurisharma0450@gmail.com>
2024-07-13 21:38:25 +0530

    Next Commit

---
+++ JavaScript/mode.js
@@ -0,0 +1,11 @@
+let mode="dark";
+let color;
+if(mode==="dark"){
+    color="black";
+}else if(mode=="pink"){
+    color="pink";
+}else{
+    color="white";
+
+}
+console.log(color);
--- javasript-basics/Baiscs/oddEven.js
+++
@@ -1,8 +0,0 @@
-let num=10;
-if(num%2==0){
-console.log(num," is even");
-}
-else{
-    console.log(num," is odd");
-}
-
